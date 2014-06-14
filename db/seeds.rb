5390217bf3f117d7f7bbe063de1ac361cd646b9b
jpark1120 <parkj1120@gmail.com>
2014-06-14 11:16:11 -0400

    made an api call method in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def movie_info(title, year=nil)
+   response_hash = JSON.parse(HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{year}").body)
+end
