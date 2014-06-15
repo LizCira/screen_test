b6a7f8b662761c00de252d0d01e10b581ed4f9aa
jpark1120 <parkj1120@gmail.com>
2014-06-15 12:32:13 -0400

    commit for potential merge conflict with liz

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,21 @@ Rails.application.routes.draw do
   end
 
 end
+
+ #     movie_likes GET    /movies/:movie_id/likes(.:format)          likes#index
+ #                 POST   /movies/:movie_id/likes(.:format)          likes#create
+ #  new_movie_like GET    /movies/:movie_id/likes/new(.:format)      likes#new
+ # edit_movie_like GET    /movies/:movie_id/likes/:id/edit(.:format) likes#edit
+ #      movie_like GET    /movies/:movie_id/likes/:id(.:format)      likes#show
+ #                 PATCH  /movies/:movie_id/likes/:id(.:format)      likes#update
+ #                 PUT    /movies/:movie_id/likes/:id(.:format)      likes#update
+ #                 DELETE /movies/:movie_id/likes/:id(.:format)      likes#destroy
+ #          movies GET    /movies(.:format)                          movies#index
+ #                 POST   /movies(.:format)                          movies#create
+ #       new_movie GET    /movies/new(.:format)                      movies#new
+ #      edit_movie GET    /movies/:id/edit(.:format)                 movies#edit
+ #           movie GET    /movies/:id(.:format)                      movies#show
+ #                 PATCH  /movies/:id(.:format)                      movies#update
+ #                 PUT    /movies/:id(.:format)                      movies#update
+ #                 DELETE /movies/:id(.:format)                      movies#destroy
+
