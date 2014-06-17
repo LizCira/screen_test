4e5a3a236c7377017e07e84a58225c82b4eb35fe
LizCira <liz.cira.goldstein@gmail.com>
2014-06-17 12:46:18 -0400

    API function now only references a user's most recent 5 likes instead of all of them

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -38,7 +38,7 @@ LikesCollection.prototype.create = function(likeParams){
     console.log(data);
     console.log(data.movie_id);
     // return data isn't being utilized but likesCollection length is used to trigger chart generation
-    if(self.models.length === 5){
+    if(self.models.length > 4){
       generateChart();
       moviesCollection.fetch();
     }
