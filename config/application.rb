267f3f5550782ab8a8c038a0d9b1363c3a7dbc7e
jpark1120 <parkj1120@gmail.com>
2014-06-18 17:47:40 -0400

    set precompile to false

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ScreenTest
   class Application < Rails::Application
+    conﬁg.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
